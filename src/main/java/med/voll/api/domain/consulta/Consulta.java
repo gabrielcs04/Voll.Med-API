439d910908d2bb578e4f769976c1b72a39407686
Gabriel Costa da Silva <gabriel.costasilva04@gmail.com>
2025-03-15 16:25:25 -0300

    Creating functionality to appointment scheduling

---
+++ src/main/java/med/voll/api/domain/consulta/Consulta.java
@@ -0,0 +1,66 @@
+package med.voll.api.domain.consulta;
+
+import jakarta.persistence.*;
+import med.voll.api.domain.medico.Medico;
+import med.voll.api.domain.paciente.Paciente;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+@Table(name = "consultas")
+@Entity(name = "Consulta")
+public class Consulta {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "medico_id")
+    private Medico medico;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "paciente_id")
+    private Paciente paciente;
+
+    private LocalDateTime data;
+
+    public Consulta() {
+    }
+
+    public Consulta(Long id, Medico medico, Paciente paciente, LocalDateTime data) {
+        this.id = id;
+        this.medico = medico;
+        this.paciente = paciente;
+        this.data = data;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Medico getMedico() {
+        return medico;
+    }
+
+    public Paciente getPaciente() {
+        return paciente;
+    }
+
+    public LocalDateTime getData() {
+        return data;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Consulta consulta = (Consulta) o;
+        return Objects.equals(id, consulta.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+}
